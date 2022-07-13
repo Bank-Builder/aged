70b8607d0418437cb289340543311744a599d7e8
Bank-Builder <andrew.turpin@cyber-mint.com>
2022-07-14 00:16:05 +0200

    fixed installation & ver

--- qed-install.sh
+++ qed-install.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 mkdir -p /home/$USER/.local/share/qed
-cp . /home/$USER/.local/share/qed
-ln /home/$USER/.local/bin/qed /home/$USER/.local/share/qed/qed.sh
-ln /home/$USER/.local/bin/qed-remove /home/$USER/.local/share/qed/qed-remove.sh
+cp qed-install.sh /home/$USER/.local/share/qed/.
+cp qed-remove.sh /home/$USER/.local/share/qed/.
+cp qed.sh /home/$USER/.local/share/qed/.
+cp qed.conf /home/$USER/.local/share/qed/.
+cp README.md /home/$USER/.local/share/qed/.
+
+ln /home/$USER/.local/share/qed/qed.sh /home/$USER/.local/bin/qed 
+ln /home/$USER/.local/share/qed/qed-remove.sh /home/$USER/.local/bin/qed-remove
 chmod +x /home/$USER/.local/share/qed/qed.sh
-echo "QED $(cat ver) installed"
+echo "QED installed"
